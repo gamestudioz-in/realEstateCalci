60bb09ecf8f2c4de047e779a74b445bb71b461ba
gamestudioz-in <arealgmr@gmail.com>
2025-04-06 19:58:34 +0530

    Ready Reckoner Rates updated for Kharghar 2025-26

--- Kharghar/shop.js
+++ Kharghar/shop.js
@@ -19,100 +19,100 @@ document.getElementById("showStampDuty").style.display = "inline-block";
   ghasara = document.querySelector("input[name='depghas']").value;
 // Sector wise values
 if (sectorNo == 1) {
-    comiValue = 115000;
-    landValue = 40500;
+    comiValue = 126500;
+    landValue = 41700;
 }
 else if (sectorNo == 2) {
-    comiValue = 152300;
+    comiValue = 167500;
     landValue = 45100;
 }
 else if (sectorNo == 3) {
-    comiValue = 115100;
+    comiValue = 132400;
     landValue = 35500;
 }
 else if (sectorNo == 4) {
-    comiValue = 140100;
+    comiValue = 154100;
     landValue = 42300;
 }
 else if (sectorNo == 5) {
-    comiValue = 151500;
-    landValue = 45700;
+    comiValue = 166700;
+    landValue = 48000;
 }
 else if (sectorNo == 6) {
-    comiValue = 168500;
-    landValue = 53900;
+    comiValue = 193800;
+    landValue = 57700;
 }
 else if (sectorNo == 7) {
-    comiValue = 158500;
-    landValue = 48600;
+    comiValue = 182300;
+    landValue = 52000;
 }
 else if (sectorNo == 8) {
-    comiValue = 148300;
+    comiValue = 169100;
     landValue = 46900;
 }
 else if (sectorNo == 9) {
     comiValue = 126700;
-    landValue = 43900;
+    landValue = 44800;
 }
 else if (sectorNo == 10) {
-    comiValue = 135800;
+    comiValue = 156200;
     landValue = 45400;
 }
 else if (sectorNo == 11) {
-    comiValue = 136200;
+    comiValue = 163400;
     landValue = 42500;
 }
 else if (sectorNo == 12) {
-    comiValue = 146900;
-    landValue = 48900;
+    comiValue = 171900;
+    landValue = 53800;
 }
 else if (sectorNo == 13) {
-    comiValue = 108500;
-    landValue = 32700;
+    comiValue = 123700;
+    landValue = 35000;
 }
 else if (sectorNo == 14) {
-    comiValue = 137300;
+    comiValue = 151000;
     landValue = 39300;
 }
 else if (sectorNo == 15) {
-    comiValue = 127600;
-    landValue = 42400;
+    comiValue = 151800;
+    landValue = 44500;
 }
 else if (sectorNo == 16) {
-    comiValue = 102000;
-    landValue = 24600;
+    comiValue = 117300;
+    landValue = 26300;
 }
 else if (sectorNo == 17) {
-    comiValue = 98100;
+    comiValue = 112800;
     landValue = 24400;
 }
 else if (sectorNo == 18) {
-    comiValue = 124700;
+    comiValue = 143400;
     landValue = 32000;
 }
 else if (sectorNo == 19) {
-    comiValue = 140200;
-    landValue = 44900;
+    comiValue = 154200;
+    landValue = 45800;
 }
 else if (sectorNo == 20) {
-    comiValue = 133500;
+    comiValue = 158900;
     landValue = 44400;
 }
 else if (sectorNo == 21) {
-    comiValue = 125600;
-    landValue = 45600;
+    comiValue = 144400;
+    landValue = 50200;
 }
 else if (sectorNo == 22) {
-    comiValue = 62200;
-    landValue = 15700;
+    comiValue = 64700;
+    landValue = 16000;
 }
 else if (sectorNo == 23) {
-    comiValue = 100100;
-    landValue = 28100;
+    comiValue = 110100;
+    landValue = 29500;
 }
 else if (sectorNo == 24) {
     comiValue = 91300;
-    landValue = 30300;
+    landValue = 30900;
 }
 else if (sectorNo == 25) {
     comiValue = 71600;
@@ -123,19 +123,19 @@ else if (sectorNo == 26) {
     landValue = 17000;
 }
 else if (sectorNo == 27) {
-    comiValue = 123600;
+    comiValue = 148300;
     landValue = 42000;
 }
 else if (sectorNo == 28) {
     comiValue = 97800;
-    landValue = 31000;
+    landValue = 31600;
 }
 else if (sectorNo == 29) {
-    comiValue = 91300;
+    comiValue = 93100;
     landValue = 29000;
 }
 else if (sectorNo == 30) {
-    comiValue = 100000;
+    comiValue = 116000;
     landValue = 31000;
 }
 else if (sectorNo == 31) {
@@ -147,32 +147,32 @@ else if (sectorNo == 32) {
     landValue = 30700;
 }
 else if (sectorNo == 33) {
-    comiValue = 101200;
-    landValue = 30700;
+    comiValue = 111300;
+    landValue = 38600;
 }
 else if (sectorNo == 34) {
-    comiValue = 123500;
+    comiValue = 147000;
     landValue = 35100;
 }
 else if (sectorNo == 35) {
-    comiValue = 122400;
-    landValue = 36600;
+    comiValue = 142000;
+    landValue = 38400;
 }
 else if (sectorNo == 36) {
-    comiValue = 66900;
-    landValue = 14700;
+    comiValue = 80300;
+    landValue = 16200;
 }
 else if (sectorNo == 37) {
-    comiValue = 72100;
-    landValue = 19200;
+    comiValue = 77100;
+    landValue = 10200;
 }
 else if (sectorNo == 38) {
-    comiValue = 64100;
+    comiValue = 65400;
     landValue = 17200;
 }
 else if (sectorNo == 39) {
-    comiValue = 88600;
-    landValue = 25000;
+    comiValue = 97500;
+    landValue = 26800;
 }
 else if (sectorNo == 40) {
     comiValue = 81300;
